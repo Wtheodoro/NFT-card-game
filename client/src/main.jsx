4301f92d2a3f39402abff7d2fd3a6c4b6cd5f5e0
Walison Theodoro <theodoro.walison@gmail.com>
2022-11-02 12:37:02 -0300

    fix: path name

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { Home, CreateBattleome } from './page'
+import { Home, CreateBattle } from './page'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/create-battle' element={<CreateBattleome />} />
+      <Route path='/create-battle' element={<CreateBattle />} />
     </Routes>
   </BrowserRouter>
 )
