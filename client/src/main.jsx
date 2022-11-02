54be6c053dc22309b3966dc06222b5bc0802443d
Walison Theodoro <theodoro.walison@gmail.com>
2022-11-02 12:32:23 -0300

    chore: init create battle and home page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Home from './page/Home';
-import './index.css';
+import { Home, CreateBattleome } from './page'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/create-battle' element={<CreateBattleome />} />
     </Routes>
-  </BrowserRouter>,
-);
+  </BrowserRouter>
+)
